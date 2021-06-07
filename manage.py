7cc912bc8fe3e58472a8eacb5a6aaa9800dd4069
Ubuntu <ubuntu@ip-172-31-8-13.ap-northeast-2.compute.internal>
2021-06-07 15:29:04 +0000

    배포용 코드 수정

--- manage.py
+++ manage.py
@@ -18,4 +18,4 @@ api.add_namespace(Todo, '/todos')# 튜토리얼
 api.add_namespace(File, '/file')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8080)
--- practice/app_start.py
+++
@@ -1,4 +0,0 @@
-# from demo_flask import app
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
